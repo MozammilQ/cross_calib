2e4c0453f2e8cf86c97c00e18191c0e761f4cfc0
MozammilQ <mozamilq.ai@gmail.com>
2021-08-31 02:01:06 +0530

    housekeeping

--- experiments/rabi_measurment.py
+++ experiments/rabi_measurment.py
@@ -1,25 +1,4 @@
-import numpy as np
-from qiskit import pulse
-from qiskit.circuit import Parameter
-import time
-from scipy.optimize import curve_fit
-
-def __init__(self):
-    self.KHz=1.0E+3
-    self.MHz=1.0E+6
-    self.GHz=1.0E+9
-    self.us=1.0E-6
-    self.ns=1.0E-9
-    self.scale_fact=1.0E-14
-    self.wait_time=120
-
-def x_16(x):
-    return int(x+8)-int((x+8)%16)
-
-def fit_fnx(x_val, y_val, fnx, init_params):
-    fit_params, conv=curve_fit(fnx, x_val, y_val, init_params)
-    y_fit=fnx(x_val, *fit_params)
-    return fit_params, y_fit
+from .common_funx import *
 
 def rabi_measurment(backend, rough_q_freq_Hz, qubit_n=0, mem_slot=0, rabi_points=50, drive_ampl_min=0, drive_ampl_max=0.75, drive_sigma_us=0.75, shots_per_point=1024):
     rabi_points=50
