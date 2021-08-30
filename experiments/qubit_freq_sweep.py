725184457be71b2c85126ced8356c81e0ef0f13e
MozammilQ <mozamilq.ai@gmail.com>
2021-08-31 01:57:59 +0530

    housekeeping

--- experiments/qubit_freq_sweep.py
+++ experiments/qubit_freq_sweep.py
@@ -1,26 +1,4 @@
-import numpy as np
-from qiskit import pulse
-from qiskit.circuit import Parameter
-import time
-from scipy.optimize import curve_fit
-
-def __init__(self):
-    self.KHz=1.0E+3
-    self.MHz=1.0E+6
-    self.GHz=1.0E+9
-    self.us=1.0E-6
-    self.ns=1.0E-9
-    self.scale_fact=1.0E-14
-    self.wait_time=120
-
-def x_16(x):
-    return int(x+8)-int((x+8)%16)
-
-def fit_fnx(x_val, y_val, fnx, init_params):
-    fit_params, conv=curve_fit(fnx, x_val, y_val, init_params)
-    y_fit=fnx(x_val, *fit_params)
-    return fit_params, y_fit
-
+from .common_funx import *
 
 def qubit_freq_sweep(backend, qubit_n=0, mem_slot=0, freq_span_MHz=30, freq_step_MHz=0.5, drive_sigma_us=0.75, drive_ampl=0.5, shots_per_freq=1024, wait_time=120):
     backend_defaults=backend.defaults()
