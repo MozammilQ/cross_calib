d3e119a08ffe1b3cb3ad3185add6692758b63366
MozammilQ <mozamilq.ai@gmail.com>
2021-09-01 03:36:04 +0530

    housekeeping

--- experiments/rabi_measurment.py
+++ experiments/rabi_measurment.py
@@ -1,9 +1,9 @@
 from .common_funx import *
 
-def rabi_measurment(backend, rough_q_freq_Hz, qubit_n=0, mem_slot=0, rabi_points=50, drive_ampl_min=0, drive_ampl_max=0.75, drive_sigma_us=0.75, shots_per_point=1024):
-    rabi_points=50
-    drive_ampl_min=0
-    drive_ampl_max=0.75
+def baseline_remove(values):
+    return np.array(values) - np.mean(values)
+
+def rabi_measurment(backend, rough_q_freq_Hz, qubit_n=0, mem_slot=0, rabi_points=50, drive_ampl_min=0, drive_ampl_max=0.75, drive_sigma_us=0.075,  shots_per_point=1024):
     drive_ampls=np.linspace(drive_ampl_min, drive_ampl_max, rabi_points)
     drive_ampl=Parameter('drive_amp')
     drive_sigma_sec=drive_sigma_us*us
@@ -12,36 +12,33 @@ def rabi_measurment(backend, rough_q_freq_Hz, qubit_n=0, mem_slot=0, rabi_points
         drive_duration=x_16(pulse.seconds_to_samples(drive_duration_sec))
         drive_sigma=x_16(pulse.seconds_to_samples(drive_sigma_sec))
         drive_chanl=pulse.drive_channel(qubit_n)
+        pulse.set_frequency(rough_q_freq_Hz, drive_chanl)
         gauss_pulse=pulse.Gaussian(duration=drive_duration, amp=drive_ampl, sigma=drive_sigma, name="Rabi Pulse")
         pulse.play(gauss_pulse, drive_chanl)
         pulse.measure(qubits=[qubit_n], registers=[pulse.MemorySlot(mem_slot)])
-        rabi_schedl=[rabi_sched.assign_parameters({drive_ampl: a}, inplace=False) for a in drive_ampls]
+
+    rabi_schedl=[rabi_sched.assign_parameters({drive_ampl: a}, inplace=False) for a in drive_ampls]
 
     job=backend.run(rabi_schedl, meas_level=1, meas_return='avg', shots=shots_per_point)
-
-    ##########################################
-    print("Job submitted waiting a minute")
-    time.sleep(60)
-    ##########################################
     
+    print("\nJob submitted waiting for completion\n")
+    job.wait_for_final_state(timeout=None, wait=wait_time, callback=None)
+
     stats=job.status()
-    while(stats.name!='DONE'):
-        print(f"From qubit_freq_sweep -->  {stats.value}\n")
-        time.sleep(wait_time)
-        stats=job.status()
 
-    rabi_results=job.results(timeout=120)
-    rabi_val=[]
+    if(stats.name=="DONE"):
+        rabi_results=job.results(timeout=120)
+        rabi_val=[]
+        for i in range(rabi_points):
+            rabi_val.append(rabi_results.get_memory(i)[qubit_n]*scale_factor)
 
-    for i in range(rabi_points):
-        rabi_val.append(rabi_results.get_memory(i)[qubit_n]*scale_factor)
+        rabi_val=np.real(remove_baseline(rabi_val))
 
-    rabi_val=np.real(remove_baseline(rabi_val))
-    rabi_init_param=[3, 0.1, 0.3, 0]
-    rabi_fnx=lambda x, A, B, drive_period, phi: (A*np.cos(2*np.pi*x/drive_period - phi)+B)
-    fit_params, y_fit=fit_function(drive_ampl, rabi_val, rabi_fnx, rabi_init_param)
-    drive_period=fit_params[2]
-    pi_ampl=abs(drive_period/2)
-    print(f"Pi Amplitude is --> {pi_ampl}")
+        rabi_init_param=[3, 0.1, 0.3, 0]
+        rabi_fnx=lambda x, A, B, drive_period, phi: (A*np.cos(2*np.pi*x/drive_period - phi)+B)
+        fit_params, y_fit=fit_function(drive_ampl, rabi_val, rabi_fnx, rabi_init_param)
+        drive_period=fit_params[2]
+        pi_ampl=abs(drive_period/2)
+        return pi_ampl
 
 
